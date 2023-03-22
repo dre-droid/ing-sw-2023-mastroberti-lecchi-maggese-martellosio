d9506c7469ede71aaa2d5c2b8237dd412d444ce6
DiegoLecchi <126402969+DiegoLecchi@users.noreply.github.com>
2023-03-22 16:20:44 +0100

    Added and implemented Tile and Type

---
+++ src/main/java/it/polimi/ingsw/Model/Tile.java
@@ -0,0 +1,13 @@
+package main.java.it.polimi.ingsw.Model;
+
+public class Tile {
+    private Type type;
+
+    public Tile(Type type) {
+        this.type = type;
+    }
+
+    public Type getType() {
+        return type;
+    }
+}
---
+++ src/main/java/it/polimi/ingsw/Model/Type.java
@@ -0,0 +1,10 @@
+package main.java.it.polimi.ingsw.Model;
+
+public enum Type {
+    CAT,
+    BOOK,
+    GAME,
+    FRAME,
+    TROPHY,
+    PLANT
+}
