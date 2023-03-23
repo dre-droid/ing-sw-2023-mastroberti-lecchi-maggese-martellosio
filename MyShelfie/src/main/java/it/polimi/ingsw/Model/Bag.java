d8a0fbc91b74b03d9cbfdd859b9b74d2b74eaebc
FrancescoMartellosio <francesco.martellosio@mail.polimi.it>
2023-03-23 16:05:51 +0100

    Classe Bag con relativi casi di test

--- MyShelfie/src/main/java/it/polimi/ingsw/Model/Bag.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/Model/Bag.java
@@ -6,14 +6,19 @@ public class Bag {
     private List<Tile> tiles;
 
     public Bag(){
+        this.tiles = new ArrayList<Tile>();
         for(Type type: Type.values()){
-            for(int i = 0; i<7;i++){
+            for(int i = 0; i<22;i++){
                 Tile tile = new Tile(type);
                 tiles.add(tile);
             }
         }
     }
 
+    /**
+     * This methods returns all the Tiles present in the bag
+     * @return All the tiles present in the Bag
+     */
     public List<Tile> getAllTiles(){
         List<Tile> allTiles = new ArrayList<Tile>();
         for(int i = 0;i<tiles.size();i++){
@@ -21,4 +26,30 @@ public class Bag {
         }
         return allTiles;
     }
+
+    public int getSize(){
+        return tiles.size();
+    }
+
+    /**
+     * This method remove a random Tile from the Bag and returns it
+     * @return a random Tile from the bag
+     */
+    public Tile pickRandomTile(){
+        Random random = new Random();
+        int randomIndex = random.nextInt(this.getSize());
+        Tile t = tiles.get(randomIndex);
+        tiles.remove(randomIndex);
+        return t;
+    }
+
+    /**
+     * This method reinserts a Tile inside the bag (at the end of the turn if the board needs to be refilled, all the isolated tiles
+     * on the board are shuffled back into the Bag
+     * @param t it's the tile that has to be reinserted into the Bag
+     */
+    public void reinsertTile(Tile t){
+        this.tiles.add(t);
+    }
+
 }
---
+++ MyShelfie/src/test/test/java/it/polimi/ingsw/Model/BagTest.java
@@ -0,0 +1,56 @@
+package test.java.it.polimi.ingsw.Model;
+
+import main.java.it.polimi.ingsw.Model.Bag;
+import main.java.it.polimi.ingsw.Model.Tile;
+import main.java.it.polimi.ingsw.Model.Type;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BagTest {
+    @Test
+    public void BagConstructor_ListNotNull(){
+        Bag bag = new Bag();
+        assertNotNull(bag);
+    }
+
+    @Test
+    public void BagConstructor_Contains132Tiles(){
+        Bag bag = new Bag();
+        assertEquals(132, bag.getAllTiles().size());
+    }
+    @Test
+    public void BagConstructor_Contains22OfEachTileType(){
+        Bag bag = new Bag();
+        boolean problem=false;
+        for(Type type: Type.values()){
+            if(bag.getAllTiles().stream().filter(t->t.getType()==type).count()!=22)
+                problem = true;
+        }
+        assertFalse(problem);
+    }
+
+    @Test
+    public void PickRandomTile_TileReturnedIsNotNull(){
+        Bag bag = new Bag();
+        assertNotNull(bag.pickRandomTile());
+    }
+    @Test
+    public void PickRandomTile_BagSizeHasDecreasedByOne(){
+        Bag bag = new Bag();
+        int sizeBefore = bag.getSize();
+        bag.pickRandomTile();
+        int sizeAfter = bag.getSize();
+        assertEquals(sizeBefore-1,sizeAfter);
+    }
+
+    @Test
+    public void ReinsertTile_TileIsInsertedCorrectly(){
+        Bag bag = new Bag();
+        Tile tile = new Tile(Type.CAT);
+        int numOfCatTilesBefore = (int) bag.getAllTiles().stream().filter(t->t.getType()==Type.CAT).count();
+        bag.reinsertTile(tile);
+        int numOfCatTilesAfter = (int) bag.getAllTiles().stream().filter(t->t.getType()==Type.CAT).count();
+        assertEquals(numOfCatTilesBefore+1,numOfCatTilesAfter);
+    }
+}
