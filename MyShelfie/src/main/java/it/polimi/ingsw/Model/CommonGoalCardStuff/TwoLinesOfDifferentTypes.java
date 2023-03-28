837a636e664f44682ce3f5529f84950a99a327cc
SaverioMaggese99 <126009586+SaverioMaggese99@users.noreply.github.com>
2023-03-28 14:20:23 +0200

    implemented strategy TwoLinesOfDifferentTypes for the commongoalcard strategy

---
+++ MyShelfie/src/main/java/it/polimi/ingsw/Model/CommonGoalCardStuff/TwoLinesOfDifferentTypes.java
@@ -0,0 +1,42 @@
+package main.java.it.polimi.ingsw.Model.CommonGoalCardStuff;
+import main.java.it.polimi.ingsw.Model.*;
+
+/**
+ * @author Saverio Maggese
+ * checks if there are two raws containing tiles of all different types
+ */
+public class TwoLinesOfDifferentTypes implements StrategyCommonGoal{
+    public boolean executeStrategy(Shelf shelf) {
+        Tile[][] shelfGrid = shelf.getGrid();
+        boolean repeated=false;
+        int rawcount=0;
+
+        for(int raw=0;raw<5;raw++){
+            for(int i=0;i<5;i++){
+                for(int j=i+1;j<5;j++){
+                    if(shelfGrid[raw][i].getType() == shelfGrid[raw][j].getType()){
+                        repeated=true;
+
+
+
+                    }
+
+                }
+            }
+            if (!repeated)
+                rawcount++;
+            repeated = false;
+
+        }
+        if (rawcount>=2)
+            return true ;
+        else
+            return false;
+
+
+
+
+
+
+    }
+}
