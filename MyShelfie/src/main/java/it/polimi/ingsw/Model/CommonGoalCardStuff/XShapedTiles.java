78550bfbcc0f0425695e4d17be9b77bb48eb2df8
SaverioMaggese99 <126009586+SaverioMaggese99@users.noreply.github.com>
2023-03-28 16:18:49 +0200

    XShaped CommonGoalCard commit

---
+++ MyShelfie/src/main/java/it/polimi/ingsw/Model/CommonGoalCardStuff/XShapedTiles.java
@@ -0,0 +1,31 @@
+package main.java.it.polimi.ingsw.Model.CommonGoalCardStuff;
+
+import main.java.it.polimi.ingsw.Model.Shelf;
+import main.java.it.polimi.ingsw.Model.Tile;
+import main.java.it.polimi.ingsw.Model.*;
+
+public class XShapedTiles implements StrategyCommonGoal {
+    @Override
+    public boolean executeStrategy(Shelf shelf) {
+        Tile[][] shelfGrid = shelf.getGrid();
+        for(int i = 1;i < 5;i++){
+            for(int j = 1;j < 4;j++){
+                if( shelfGrid[i][j]!= null && shelfGrid[i+1][j+1]!= null && shelfGrid[i-1][j-1]!= null && shelfGrid[i+1][j-1]!= null && shelfGrid[i-1][j+1]!= null){
+                    if(shelfGrid[i][j].getType()==shelfGrid[i+1][j+1].getType()){
+                        if(shelfGrid[i][j].getType()==shelfGrid[i-1][j-1].getType()){
+                            if(shelfGrid[i][j].getType()==shelfGrid[i+1][j-1].getType()){
+                                if(shelfGrid[i][j].getType()==shelfGrid[i-1][j+1].getType()){
+                                    return true;
+                                }
+                            }
+                        }
+                    }
+
+                }
+
+            }
+        }
+        return false;
+
+    }
+}
