c58c603c665435fb57c394b1b3d1cfedac85467b
SaverioMaggese99 <126009586+SaverioMaggese99@users.noreply.github.com>
2023-03-28 15:23:15 +0200

    corrected 8ofthesametype and 4cornersametype

--- MyShelfie/src/main/java/it/polimi/ingsw/Model/CommonGoalCardStuff/EightofSameType.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/Model/CommonGoalCardStuff/EightofSameType.java
@@ -19,15 +19,16 @@ public class EightofSameType implements StrategyCommonGoal{
         int countPlant=0;
         for(int i=0;i<5;i++){
             for(int j=0;j<6;j++){
-                switch (shelfGrid[i][j].getType()) {
-                    case CAT:countCat++;
-                    case BOOK:countBook++;
-                    case GAME:countGame++;
-                    case FRAME:countFrame++;
-                    case PLANT:countPlant++;
-                    case TROPHY:countTrophy++;
+                if(shelfGrid[i][j].getType()!= null)
+                    switch (shelfGrid[i][j].getType()) {
+                        case CAT:countCat++;
+                        case BOOK:countBook++;
+                        case GAME:countGame++;
+                        case FRAME:countFrame++;
+                        case PLANT:countPlant++;
+                        case TROPHY:countTrophy++;
 
-                }
+                    }
                 if(countCat >=8 || countBook>=8 || countFrame>=8 || countTrophy>=8 || countPlant>=8 || countGame>=8){
                     return true;
                 }
--- MyShelfie/src/main/java/it/polimi/ingsw/Model/CommonGoalCardStuff/FourCornerOfTheSameType.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/Model/CommonGoalCardStuff/FourCornerOfTheSameType.java
@@ -6,10 +6,11 @@ public class FourCornerOfTheSameType implements StrategyCommonGoal{
     @Override
     public boolean executeStrategy(Shelf shelf) {
         Tile[][] shelfGrid = shelf.getGrid();
-        if(shelfGrid[0][0].getType()==shelfGrid[0][4].getType())
-            if(shelfGrid[0][4].getType()==shelfGrid[5][0].getType())
-                if(shelfGrid[5][0].getType()==shelfGrid[5][4].getType())
-                    return true;
+        if(shelfGrid[0][0].getType()!= null && shelfGrid[0][4].getType()!=null && shelfGrid[5][0].getType()!=null && shelfGrid[5][4].getType()!= null){
+            if(shelfGrid[0][0].getType()==shelfGrid[0][4].getType())
+                if(shelfGrid[0][4].getType()==shelfGrid[5][0].getType())
+                    if(shelfGrid[5][0].getType()==shelfGrid[5][4].getType())
+                        return true;}
         return false;
-    }
+        }
 }
