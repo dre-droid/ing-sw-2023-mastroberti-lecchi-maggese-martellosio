b57ecb9ed4ea2b48a4738cb0d5b42b0c9d5d0703
SaverioMaggese99 <126009586+SaverioMaggese99@users.noreply.github.com>
2023-06-17 14:41:38 +0200

    correction in 2 squares of the same type  cgc

--- MyShelfie/src/main/java/it/polimi/ingsw/Model/CommonGoalCardStuff/SquaredShapedGroups.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/Model/CommonGoalCardStuff/SquaredShapedGroups.java
@@ -17,14 +17,14 @@ public class SquaredShapedGroups implements StrategyCommonGoal, Serializable {
             for(int raw=0;raw<5;raw++){
                 if(shelfGrid[raw][col] != null && shelfGrid[raw+1][col+1] != null && shelfGrid[raw][col+1] != null && shelfGrid[raw+1][col] != null){
                     if(shelfGrid[raw][col].getType() == shelfGrid[raw+1][col+1].getType() && shelfGrid[raw][col].getType() == shelfGrid[raw][col+1].getType() && shelfGrid[raw][col].getType() == shelfGrid[raw+1][col].getType()){
-
+                        boolean adjFound = false;
                         sqx=raw;
                         sqy=col;
                         for(int y=sqy;y<4;y++){
                             for(int x=sqx;x<5;x++){
                                 if(shelfGrid[x][y] != null && shelfGrid[x+1][y+1] != null && shelfGrid[x][y+1] != null && shelfGrid[x+1][y] != null){
                                     if(shelfGrid[x][y].getType() == shelfGrid[x+1][y+1].getType()  && shelfGrid[x][y].getType() == shelfGrid[x+1][y].getType()  && shelfGrid[x][y+1].getType() == shelfGrid[x][y].getType()){
-                                        if((Math.abs(x-sqx) > 2)  || (Math.abs(y-sqy) > 2)){
+                                        if((Math.abs(x-sqx) > 1)  || (Math.abs(y-sqy) > 1)){
                                             return true;
                                         }
                                     }
