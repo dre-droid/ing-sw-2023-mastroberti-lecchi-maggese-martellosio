ea200645cce202e2fb5481d60a79e93055d20deb
SaverioMaggese99 <126009586+SaverioMaggese99@users.noreply.github.com>
2023-06-22 16:57:07 +0200

    IncreasingOrDecreasingHeight modificata per la 3milionesimae1volta.

--- MyShelfie/src/main/java/it/polimi/ingsw/Model/CommonGoalCardStuff/IncreasingOrDecreasingHeight.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/Model/CommonGoalCardStuff/IncreasingOrDecreasingHeight.java
@@ -10,62 +10,87 @@ public class IncreasingOrDecreasingHeight implements StrategyCommonGoal, Seriali
     @Override
     public boolean executeStrategy(Shelf shelf) {
         Tile[][] grid = shelf.getGrid();
-        int height = grid.length;
-        int width = grid[0].length;
+        int height = 6;
+        int width = 5;
         boolean columnDesc6 = true;
+        boolean nullaboveDesc6 = false;
         boolean columnDesc5 = true;
+        boolean nullaboveDesc5 = false;
         boolean columnAsc6 = true;
+        boolean nullaboveAsc6 = false;
         boolean columnAsc5 = true;
+        boolean nullaboveAsc5 = false;
         // Check descending height=6
         for (int col = 0; col < width; col++) {
-            for (int row = 0; row < height - col; row++) {
+            for (int row = 5; row >= col; row--) {
                 if (grid[row][col] == null) {
                     columnDesc6 = false;
                     break;
                 }
             }
-            if (columnDesc6) {
-                return true;
-            }
+
+        }
+        if(grid[0][1] == null && grid[1][2] == null && grid[2][3] == null && grid[3][4] == null){
+            nullaboveDesc6 = true;
+
+        }
+        if (columnDesc6 && nullaboveDesc6) {
+            return true;
         }
 
         // Check descending height=5
         for (int col = 0; col < width; col++) {
-            for (int row = 0; row < height - 1 - col; row++) {
+            for (int row = 5; row > col; row--) {
                 if (grid[row][col] == null) {
                     columnDesc5 = false;
                     break;
                 }
+
             }
-            if (columnDesc5) {
-                return true;
-            }
+
+        }
+        if(grid[0][0] == null && grid[1][1] == null && grid[2][2] == null && grid[3][3] == null && grid[4][4] == null){
+            nullaboveDesc5 = true;
+
+        }
+        if (columnDesc5 && nullaboveDesc5) {
+            return true;
         }
 
         // Check ascending height=6
         for (int col = 0; col < width; col++) {
-            for (int row = 0; row <= col; row++) {
+            for (int row = 5; row >= 4 - col; row--) {
                 if (grid[row][col] == null) {
                     columnAsc6 = false;
                     break;
                 }
             }
-            if (columnAsc6) {
-                return true;
-            }
+
+        }
+        if(grid[3][0] == null && grid[2][1] == null && grid[1][2] == null && grid[0][3] == null){
+            nullaboveAsc6 = true;
+
+        }
+        if (columnAsc6 && nullaboveAsc6) {
+            return true;
         }
 
         // Check ascending height=5
         for (int col = 0; col < width; col++) {
-            for (int row = 0; row <= col + 1; row++) {
+            for (int row = 5; row > 4 - col; row--) {
                 if (grid[row][col] == null) {
                     columnAsc5 = false;
                     break;
                 }
             }
-            if (columnAsc5) {
-                return true;
-            }
+
+        }
+        if(grid[4][0] == null && grid[3][1] == null && grid[2][2] == null && grid[1][3] == null && grid[0][4]==null){
+            nullaboveAsc5 = true;
+
+        }
+        if (columnAsc5 && nullaboveAsc5) {
+            return true;
         }
 
         return false;  // None of the conditions satisfied
